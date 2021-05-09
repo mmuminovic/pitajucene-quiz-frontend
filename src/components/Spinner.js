6ad9ff555036b91d2272d609f2bb2f7509c84614
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2021-05-09 14:59:01 +0200

    spinner color changed

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Spinner } from 'reactstrap'
+import * as colors from '../styles/main.scss'
 
 const Loader = () => (
     <Spinner
         type="grow"
-        style={{ color: '#d8dffd', width: '70px', height: '70px' }}
+        style={{ color: colors.secondary, width: '70px', height: '70px' }}
     />
 )
 
